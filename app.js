e5f603cd419c6c6156e48da73908fc0356bb7d2f
edevelopk20@gmail.com <edwardh1995@yahoo.com>
2024-03-25 19:00:40 -0400

    added difficulty, selected starter

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 const pokemon = require('./data.js')
+
+const game = {
+  party: [],
+  gyms: [
+    { location: "Pewter City", completed: false, difficulty: 1 },
+    { location: "Cerulean City", completed: false, difficulty: 2 },
+    { location: "Vermilion City", completed: false, difficulty: 3 },
+    { location: "Celadon City", completed: false, difficulty: 4 },
+    { location: "Fuchsia City", completed: false, difficulty: 5 },
+    { location: "Saffron City", completed: false, difficulty: 6 },
+    { location: "Cinnabar Island", completed: false, difficulty: 7 },
+    { location: "Viridian City", completed: false, difficulty: 8 },
+  ],
+  items: [
+    { name: "potion", quantity: 4 },
+    { name: "pokeball", quantity: 8 },
+    { name: "rare candy", quantity: 99 },
+    ],
+    difficulty: [
+        { mode: "easy" },
+        { mode: "medium" },
+        { mode: "hard"} // added 3 difficulty modes
+  ]
+}
+
+game.party.push(pokemon[0]); // Selected starter Bulbasaur
+console.log(game.party)
